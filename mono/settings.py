7a3ef3cc33d6d18cbb12b16eef01f872d497691a
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-02-07 01:58:07 +0300

    DJANGO test.2.0

--- mono/settings.py
+++ mono/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'pages',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- pages/tests.py
+++ pages/tests.py
@@ -1,35 +1,88 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
 from django.urls import reverse
+
 from. models import Post
 
 # Create your tests here.
-class PostModelTest(TestCase):
+class BlogTests(TestCase):
     def setUp(self):
-        Post.objects.create(text='just a test')
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@mail.com',
+            password='secret'
+        )
 
-    def test_text_content(self):
-        post=Post.objects.get(id=1)
-        expected_object_name = f'{ post.text }'
-        self.assertEqual(expected_object_name, 'just a test')
+
+        self.post = Post.objects.create(
+            title='A good title',
+            body='Nice body content',
+            author=self.user,
+        )
 
 
-class HomePageView(TestCase):
-    def setUp(self):
-        Post.objects.create(text='just is another test')
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
+
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
 
     def test_view_url_exists_at_proper_location(self):
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_view_url_by_name(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
 
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'home.html')
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice body content')
+
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body content')
+        self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good title')
+        self.assertTemplateUsed(response, 'post_detail.html')
+
+
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_post_delete_view(self):
+        response = self.client.get(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
+
 
     def test_about_page_status_code(self):
-        resp = self.client.get('/about/')
-        self.assertEqual(resp.status_code, 200)
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
